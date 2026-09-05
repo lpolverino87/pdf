3982b6fdcaa1acbd20965826541fc4b214c29104
lpolverino87 <l.polverino87@gmail.com>
2026-09-05 12:37:00 +0200

    Prepare API serverless bundle for Vercel

--- artifacts/api-server/build.mjs
+++ artifacts/api-server/build.mjs
@@ -14,13 +14,10 @@ async function buildAll() {
   const distDir = path.resolve(artifactDir, "dist");
   await rm(distDir, { recursive: true, force: true });
 
-  await esbuild({
-    entryPoints: [path.resolve(artifactDir, "src/index.ts")],
+  const commonOptions = {
     platform: "node",
     bundle: true,
     format: "esm",
-    outdir: distDir,
-    outExtension: { ".js": ".mjs" },
     logLevel: "info",
     // Some packages may not be bundleable, so we externalize them, we can add more here as needed.
     // Some of the packages below may not be imported or installed, but we're adding them in case they are in the future.
@@ -117,6 +114,20 @@ globalThis.__filename = __bannerUrl.fileURLToPath(import.meta.url);
 globalThis.__dirname = __bannerPath.dirname(globalThis.__filename);
     `,
     },
+  };
+
+  await esbuild({
+    ...commonOptions,
+    entryPoints: [path.resolve(artifactDir, "src/index.ts")],
+    outdir: distDir,
+    outExtension: { ".js": ".mjs" },
+  });
+
+  await esbuild({
+    ...commonOptions,
+    entryPoints: [path.resolve(artifactDir, "src/serverless.ts")],
+    outdir: distDir,
+    outExtension: { ".js": ".mjs" },
   });
 }
 
